c518a22b9232b40623e951a9df505d5edfdd1736
smehrem <smehrem@hpcs03.op.umcutrecht.nl>
2019-03-25 14:02:11 +0100

    Added stuff.

--- MakeWindowPairs_genomewide.sh
+++ MakeWindowPairs_genomewide.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
+#$ -l h_rt=01:00:00
+#$ -l h_vmem=20G
+#$ -cwd
+#$ -N genomewide_windowpairs
+#$ -o logs
+#$ -e logs
+#$ -m beas
+#$ -M S.L.Mehrem-2@umcutrecht.nl
 
-$chr=$1
 
-echo 'Processing chromosome '$chr
-for caller in gridss delly lumpy manta; do
-	echo 'Processing caller '$caller
-	./channel_maker.py -b ../MinorResearchInternship/BAM/NA12878.bam -c $chr -o genomewide_windowpairs/$caller/$chr'_windowpairs_DEL' -l $chr'_channelmaker_DEL.log' -vcf ../MinorResearchInternship/VCF/$caller'.sym.vcf' -svt DEL
+conda activate breakpoint-pairs
+
+while read chr; do
+    echo 'Processing chromosome '$chr
+    for caller in gridss; do
+        echo 'Processing caller '$caller
+	./channel_maker.py -b ../NA12878/BAM/NA12878.bam  -c $chr -o genomewide_windowpairs/$caller/$chr'_windowpairs_DEL' -l genomewide_windowpairs/$caller/$chr'_channelmaker_DEL.log' -vcf ../NA12878/VCF/$caller'.sym.vcf' -svt DEL
 	echo $caller' processed.'
-done
-echo $chr' processed.'
-#done < ../MinorResearchInternship/BAM/BAM_chr_list
+    done
+    echo $chr' processed.'
+done < BAM_chr_list.txt
 
        
