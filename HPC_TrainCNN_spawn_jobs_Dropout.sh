f22a5d63a6bbd150f90fcfa11abbac6d93ae97fd
smehrem <smehrem@hpcs04.op.umcutrecht.nl>
2019-04-23 16:35:22 +0200

    For McFly dropout.

--- HPC_TrainCNN_spawn_jobs_Dropout.sh
+++ HPC_TrainCNN_spawn_jobs_Dropout.sh
@@ -3,6 +3,6 @@
 while read -a paras; do
 
 echo "Processing parameters Split:"${paras[0]}", Epochs:"${paras[1]}", Learning rate:"${paras[2]}" Caller:"${paras[3]}", Dropout1: "${paras[4]}", Dropout2: "${paras[5]}
-   qsub CNN_Train_DellyDels_Spl_Epo_LR.sh ${paras[0]} ${paras[1]} ${paras[2]} ${paras[3]} ${paras[4]} ${paras[5]}
-done <  Parameters_HPC_LR5.txt
+   qsub CNN_Train_DellyDels_Spl_Epo_LR_Dropout.sh ${paras[0]} ${paras[1]} ${paras[2]} ${paras[3]} ${paras[4]} ${paras[5]}
+done < Dropout_Test_Paras_OneLine.txt  
 
