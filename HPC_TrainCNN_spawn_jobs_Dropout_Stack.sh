05995d9f4b4155d6c9b34da8f4e2c38a91674f9a
smehrem <smehrem@cog134.op.umcutrecht.nl>
2019-05-01 17:19:06 +0200

    X

---
+++ HPC_TrainCNN_spawn_jobs_Dropout_Stack.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+while read -a paras; do
+
+echo "Processing parameters Split:"${paras[0]}", Epochs:"${paras[1]}", Learning rate:"${paras[2]}" Caller:"${paras[3]}", Dropout1: "${paras[4]}", Dropout2: "${paras[5]}", Stack:"${paras[6]}
+   qsub CNN_Train_DellyDels_Spl_Epo_LR_Dropout_Stack.sh ${paras[0]} ${paras[1]} ${paras[2]} ${paras[3]} ${paras[4]} ${paras[5]} ${paras[6]}
+done < Parameters_StackedWindows.txt 
+
