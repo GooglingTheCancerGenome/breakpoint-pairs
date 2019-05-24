20b5fe01550dfc195d36de04b40b2ab5ea15373c
smehrem <smehrem@hpcs04.op.umcutrecht.nl>
2019-05-24 17:13:33 +0200

    X

--- HPC_RunChannelImportance.sh
+++ HPC_RunChannelImportance.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+#$ -l h_rt=00:05:00
+#$ -l h_vmem=1G
+#$ -cwd
+#$ -N CNN_Train_DellyDels
+#$ -o logs_qsub
+#$ -e logs_qsub
+
+
+
 while read -a paras; do
 
 qsub ChannelImportance_Run.sh ${paras[0]}  ${paras[1]} ${paras[2]} ${paras[3]} ${paras[4]} ${paras[5]}
 
-done < ChannelImportance_Paras.txt 
+done < ChannelImportance_Paras.txt  
 
